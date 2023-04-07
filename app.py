ccc2c75139382c4d61f82f4a1d05a7135cd6104f
Dayorboye <116425062+Dayorboye@users.noreply.github.com>
2023-04-07 16:19:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -166,15 +166,15 @@ app.layout = html.Div(children=[
                                                 "height":"70vh"}),),],
                         style={"display": "flex","background-color": "#0e1012","padding-bottom": "60px","justify-content": "space-around"}),
                   
-#         html.Div(children=[
+        html.Div(children=[
                         
-#                         html.Div(dcc.Graph(id='plot4', figure = line_fig,
-#                                                 style={"background-color": "#161A1D","text-align": "center",
-#                                                 "height":"70vh"}),),
-#                         html.Div(dcc.Graph(id='plot5', figure = fig,
-#                                                 style={"background-color": "#161A1D","text-align": "center",
-#                                                 "height":"70vh"}),),],
-#                         style={"display": "flex","background-color": "#0e1012","padding-bottom": "60px","justify-content": "space-around"}), 
+                        html.Div(dcc.Graph(id='plot4',
+                                                style={"background-color": "#161A1D","text-align": "center",
+                                                "height":"70vh"}),),
+                        html.Div(dcc.Graph(id='plot5', 
+                                                style={"background-color": "#161A1D","text-align": "center",
+                                                "height":"70vh"}),),],
+                        style={"display": "flex","background-color": "#0e1012","padding-bottom": "60px","justify-content": "space-around"}), 
                                    
                      # Graph layout
                                
@@ -191,8 +191,10 @@ app.layout = html.Div(children=[
 
 @app.callback( [Output(component_id='plot1', component_property='figure'),
                Output(component_id='plot2',component_property='figure'),
-               Output(component_id='plot3',component_property='figure'),],
-               [Input(component_id='customer_type', component_property='value'),],
+               Output(component_id='plot3',component_property='figure'),
+               Output(component_id='plot4',component_property='figure'),
+               Output(component_id='plot5',component_property='figure'),],
+              [Input(component_id='customer_type', component_property='value'),],
               
               )
 # Add computation to callback function and return graph
@@ -202,6 +204,8 @@ def get_graph(Customer_type):
             
             XYZ_Company_Datasetss = XYZ_Company_Datasets.copy()
             bar_fig = px.bar(XYZ_Company_Datasets, x='Product line', y='Total', color='City', title='TOTAL SALES')
+            bar_fig1 = px.bar(XYZ_Company_Datasetss, x='Product line', y='Quantity', color='City', title='THE SUPERMARKET QUANTITY ORDER')
+            bar_fig2 = px.bar(XYZ_Company_Datasetss, x='Product line', y='gross income', color='City', title='THE SUPERMARKET GROSS INCOME')
             tree_fig = px.treemap(XYZ_Company_Datasets, 
                             path=['City', 'Product line'], 
                             values='Total',
@@ -209,9 +213,13 @@ def get_graph(Customer_type):
                             color_continuous_scale='RdBu',
                             title='PRODUCT LINE COUNT BY CITY')
             pie_fig = px.pie(XYZ_Company_Datasets, values='gross income', names='City', title='THE SUPERMARKET GROSS INCOME')
+            pie_fig1 = px.pie(XYZ_Company_Datasets, values='Quantity', names='City', title='THE SUPERMARKET QUANTITY ORDER')
+            pie_fig2 = px.pie(XYZ_Company_Datasets, values='Total', names='City', title='THE SUPERMARKET TOTAL SALES')
        else:
              XYZ_Company_Datasetss = XYZ_Company_Datasets[XYZ_Company_Datasets['Customer type'] == Customer_type]     
-             bar_fig = px.bar(XYZ_Company_Datasetss, x='Product line', y='Total', color='City', title='TOTAL SALES')
+             bar_fig = px.bar(XYZ_Company_Datasetss, x='Product line', y='Total', color='City', title='THE SUPERMARKET TOTAL SALES')
+             bar_fig1 = px.bar(XYZ_Company_Datasetss, x='Product line', y='Quantity', color='City', title='THE SUPERMARKET QUANTITY ORDER')
+             bar_fig2 = px.bar(XYZ_Company_Datasetss, x='Product line', y='gross income', color='City', title='THE SUPERMARKET GROSS INCOME')
              tree_fig = px.treemap(XYZ_Company_Datasetss, 
                             path=['City', 'Product line'], 
                             values='Total',
@@ -220,7 +228,7 @@ def get_graph(Customer_type):
                             title='PRODUCT LINE COUNT BY CITY')
              pie_fig = px.pie(XYZ_Company_Datasetss, values='gross income', names='City', title='THE SUPERMARKET GROSS INCOME')
               
-       return tree_fig,bar_fig,pie_fig
+       return tree_fig,bar_fig,pie_fig,bar_fig1, bar_fig2
 
 
 
@@ -229,6 +237,161 @@ def get_graph(Customer_type):
 if __name__ == '__main__':
       app.run_server()
 
+
+ 
+# app.layout = html.Div(children=[ 
+#                                 # TASK1: Add title to the dashboard
+
+#                                 html.H1('SUPERMARKET BRANCHES PERFORMANCE',style={'textAlign':'center','color':'#7570b3','font-size':'48'}),
+#                                 dcc.Dropdown(id='customer_type', 
+#                                                      # Update dropdown values using list comphrehension
+#                                                      options=[{
+#                                                                'label': i,
+#                                                                'value': i
+#                                                                } for i in Customer_option ],
+#                                                      placeholder="Select Customer", searchable = True , value = 'All Customers',
+#                                                      style={'width':'50%', 'padding':'3px', 'font-size': '20px', 'text-align-last' : 'center'}),
+
+#                         html.Div(children=[
+#                         html.Div(children=[html.Div(
+#                                         html.Div(html.H1("ABUJA INCOME (N)", 
+#                                                                 style={"text-align": "center","color":"#161A1D","background-color": "#d3bd98",
+#                                                                        "margin-bottom":"0","padding": "30px","font-size":"20px"
+#                                                                        ,"font-family": "Abel","font-weight":"600","font-size": "16px"
+                                                                       
+                                        
+#                                                                 }),),style={}),                       
+#                                         html.Div(
+#                                         html.Div(html.H1(abuja, 
+#                                                                 style={"text-align": "center","color":"#161A1D","background-color":"#E8DAC5",
+#                                                                        "margin-top":"0","padding": "30px","font-size":"20px"
+#                                                                        ,"font-family": "Abel","font-weight":"400","font-size": "2em"
+                                        
+#                                                                 }),),style={}),
+#                                                                 ],),
+                        
+#                           html.Div(children=[html.Div(
+#                                         html.Div(html.H1("LAGOS INCOME (N)", 
+#                                                                 style={"text-align": "center","color":"#161A1D","background-color": "#d3bd98",
+#                                                                        "margin-bottom":"0","padding": "30px","font-size":"20px"
+#                                                                        ,"font-family": "Abel","font-weight":"600","font-size": "16px"
+                                                                       
+                                        
+#                                                                 }),),style={}),                       
+#                                         html.Div(
+#                                         html.Div(html.H1(lagos, 
+#                                                                 style={"text-align": "center","color":"#161A1D","background-color":"#E8DAC5",
+#                                                                        "margin-top":"0","padding": "30px","font-size":"20px"
+#                                                                        ,"font-family": "Abel","font-weight":"400","font-size": "2em"
+                                        
+#                                                                 }),),style={}),
+#                                                                 ],),
+
+#                         html.Div(children=[html.Div(
+#                                         html.Div(html.H1("PORT HARCOURT INCOME (N)", 
+#                                                                 style={"text-align": "center","color":"#161A1D","background-color": "#d3bd98",
+#                                                                        "margin-bottom":"0","padding": "30px","font-size":"20px"
+#                                                                        ,"font-family": "Abel","font-weight":"600","font-size": "16px"
+                                                                       
+                                        
+#                                                                 }),),style={}),                       
+#                                         html.Div(
+#                                         html.Div(html.H1(pH, 
+#                                                                 style={"text-align": "center","color":"#161A1D","background-color":"#E8DAC5",
+#                                                                        "margin-top":"0","padding": "30px","font-size":"20px"
+#                                                                        ,"font-family": "Abel","font-weight":"400","font-size": "2em",
+                                                                           
+                                        
+#                                                                 }),),style={}),
+#                                                                 ],),
+#                                                 ],
+#                         style={"display": "flex","background-color": "#0e1012","padding": "30px 0 10px 0","justify-content": "space-around"}),
+         
+         
+#         html.Div(dcc.Graph(id='plot1'),
+#                                                 style={"background-color": "#161A1D",
+#                                                 "padding": "60px",},),                       # Create an outer division 
+                            
+#         html.Div(children=[
+                        
+#                         html.Div(dcc.Graph(id='plot2', 
+#                                                 style={"background-color": "#161A1D","text-align": "center",
+#                                                 "height":"70vh"}),),
+#                         html.Div(dcc.Graph(id='plot3',
+#                                                 style={"background-color": "#161A1D","text-align": "center",
+#                                                 "height":"70vh"}),),],
+#                         style={"display": "flex","background-color": "#0e1012","padding-bottom": "60px","justify-content": "space-around"}),
+                  
+#         html.Div(children=[
+                        
+#                         html.Div(dcc.Graph(id='plot4',
+#                                                 style={"background-color": "#161A1D","text-align": "center",
+#                                                 "height":"70vh"}),),
+#                         html.Div(dcc.Graph(id='plot5', 
+#                                                 style={"background-color": "#161A1D","text-align": "center",
+#                                                 "height":"70vh"}),),],
+#                         style={"display": "flex","background-color": "#0e1012","padding-bottom": "60px","justify-content": "space-around"}), 
+                                   
+#                      # Graph layout
+                               
+#                                 # html.Div(dcc.Graph(figure=tree_fig)),
+    
+#                                 # html.Div([
+#                                 #         html.Div(dcc.Graph( figure=bar_fig)),
+#                                 #         html.Div(dcc.Graph(figure=pie_fig))
+#                                 # ], style={'display': 'flex'}),
+                                 
+                                
+                            
+#                                 ],style={"height": "100vh"})
+
+# @app.callback( [Output(component_id='plot1', component_property='figure'),
+#                Output(component_id='plot2',component_property='figure'),
+#                Output(component_id='plot3',component_property='figure'),
+#                Output(component_id='plot4',component_property='figure'),
+#                Output(component_id='plot5',component_property='figure'),],
+#               [Input(component_id='customer_type', component_property='value'),],
+              
+#               )
+# # Add computation to callback function and return graph
+# def get_graph(Customer_type):
+       
+#        if Customer_type == "All Customers":
+            
+#             XYZ_Company_Datasetss = XYZ_Company_Datasets.copy()
+#             bar_fig = px.bar(XYZ_Company_Datasets, x='Product line', y='Total', color='City', title='TOTAL SALES')
+#             bar_fig1 = px.bar(XYZ_Company_Datasetss, x='Product line', y='Quantity', color='City', title='THE SUPERMARKET QUANTITY ORDER')
+#             bar_fig2 = px.bar(XYZ_Company_Datasetss, x='Product line', y='gross income', color='City', title='THE SUPERMARKET GROSS INCOME')
+#             tree_fig = px.treemap(XYZ_Company_Datasets, 
+#                             path=['City', 'Product line'], 
+#                             values='Total',
+#                             color='Total',
+#                             color_continuous_scale='RdBu',
+#                             title='PRODUCT LINE COUNT BY CITY')
+#             pie_fig = px.pie(XYZ_Company_Datasets, values='gross income', names='City', title='THE SUPERMARKET GROSS INCOME')
+#             pie_fig1 = px.pie(XYZ_Company_Datasets, values='Quantity', names='City', title='THE SUPERMARKET QUANTITY ORDER')
+#             pie_fig2 = px.pie(XYZ_Company_Datasets, values='Total', names='City', title='THE SUPERMARKET TOTAL SALES')
+#        else:
+#              XYZ_Company_Datasetss = XYZ_Company_Datasets[XYZ_Company_Datasets['Customer type'] == Customer_type]     
+#              bar_fig = px.bar(XYZ_Company_Datasetss, x='Product line', y='Total', color='City', title='THE SUPERMARKET TOTAL SALES')
+#              bar_fig1 = px.bar(XYZ_Company_Datasetss, x='Product line', y='Quantity', color='City', title='THE SUPERMARKET QUANTITY ORDER')
+#              bar_fig2 = px.bar(XYZ_Company_Datasetss, x='Product line', y='gross income', color='City', title='THE SUPERMARKET GROSS INCOME')
+#              tree_fig = px.treemap(XYZ_Company_Datasetss, 
+#                             path=['City', 'Product line'], 
+#                             values='Total',
+#                             color='Total',
+#                             color_continuous_scale='RdBu',
+#                             title='PRODUCT LINE COUNT BY CITY')
+#              pie_fig = px.pie(XYZ_Company_Datasetss, values='gross income', names='City', title='THE SUPERMARKET GROSS INCOME')
+              
+#        return tree_fig,bar_fig,pie_fig,bar_fig1, bar_fig2
+
+
+
+
+# # Run the app
+# if __name__ == '__main__':
+#       app.run_server()
 
 
 
